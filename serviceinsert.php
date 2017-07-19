3d4bd1ca3a0a8802beb529b22d951d6e0b2b2091
Pierluigi De Stasio <peterdestasio@gmail.com>
2017-07-18 21:08:55 -0400

    added webservices

---
+++ serviceinsert.php
@@ -0,0 +1,51 @@
+<?php
+$host = "localhost"; //database host server
+$db = "fritter"; //database name
+$user = "root"; //database user
+$pass = ""; //password
+$connection = mysql_connect($host, $user, $pass);
+//Check to see if we can connect to the server
+
+/*
+$x = $_GET['x'];
+$y = $_GET['y'];
+$z = $_GET['z'];
+*/
+
+$user_name = $_POST['user_name'];
+$user_email = $_POST['user_email'];
+$user_pass = $_POST['user_pass'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$birthday = $_POST['birthday'];
+$city = $_POST['city'];
+$credit_card = $_POST['credit_card'];
+$ccv = $_POST['ccv'];
+
+if(!$connection)
+{
+    die("Database server connection failed.");  
+}
+
+
+
+else
+{
+    //Attempt to select the database
+    $dbconnect = mysql_select_db("fritter", $connection);
+    //Check to see if we could select the database
+    if(!$dbconnect)
+    {
+        die("Unable to connect to the specified database!");
+    }
+    else
+    {
+        $query = "INSERT INTO users(user_name,user_email,user_pass,first_name,last_name,birthday,city,credit_card,ccv) 
+                                                       VALUES('$user_name' ,'$user_email','$user_pass','$first_name','$last_name','$birthday','$city','$credit_card','$ccv')";
+      
+        $resultset = mysql_query($query, $connection);
+        echo "Successfully added ";
+        echo $query;
+    }
+}
+?>
---
+++ serviceselect.php
@@ -0,0 +1,35 @@
+<?php
+$host = "localhost"; //database host server
+$db = "fritter"; //database name
+$user = "root"; //database user
+$pass = ""; //password
+$connection = mysql_connect($host, $user, $pass);
+//Check to see if we can connect to the server
+if(!$connection)
+{
+    die("Database server connection failed.");  
+}
+else
+{
+    //Attempt to select the database
+    $dbconnect = mysql_select_db("fritter", $connection);
+    //Check to see if we could select the database
+    if(!$dbconnect)
+    {
+        die("Unable to connect to the specified database!");
+    }
+    else
+    {
+        $query = "SELECT * FROM users";
+        $resultset = mysql_query($query, $connection);
+        $records = array();
+        //Loop through all our records and add them to our array
+        while($r = mysql_fetch_assoc($resultset))
+        {
+            $records[] = $r;        
+        }
+        //Output the data as JSON
+        echo json_encode($records);
+    }
+}
+?>
